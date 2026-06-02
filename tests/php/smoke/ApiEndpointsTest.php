2774005e8a799b689a1e27ac7953a6092da37cc4
IvchenkoAlexandr <aleksandr.ivchenko19@gmail.com>
2026-06-02 15:59:56 +0600

    ATK-180 improve spec and smoke test

--- tests/php/smoke/ApiEndpointsTest.php
+++ tests/php/smoke/ApiEndpointsTest.php
@@ -3788,6 +3788,7 @@ class ApiEndpointsTest extends TestCase
 
         $this->assertReachable($this->client->get($base . '/' . self::TEST_UUID . '/files'));
         $this->assertReachable($this->client->post($base . '/' . self::TEST_UUID . '/files', ['json' => [['filename' => 'X']],]));
+        $this->assertReachable($this->client->get($base . '/' . self::TEST_UUID . '/files/' . self::TEST_UUID));
         $this->assertReachable($this->client->delete($base . '/' . self::TEST_UUID . '/files/' . self::TEST_UUID));
 
         $this->assertReachable($this->client->post($base . '/delete', ['json' => [['meta' => ['href' => 'x']]]]));
